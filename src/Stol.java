aec947c09f323e887b63d127a19cf44e565a3cf4
iosefstalin <jelleholm91@gmail.com>
2019-04-04 12:46:00 +0200

    Ny class, og metode "erdugrim?"

---
+++ src/Stol.java
@@ -0,0 +1,13 @@
+public class Stol {
+
+    private int ben;
+    private int bordpladeType;
+
+    public int getBen() {
+        return ben;
+    }
+
+    public int getBordpladeType() {
+        return bordpladeType;
+    }
+}
